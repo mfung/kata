c41202e3f8f2b4341c95a9a7ab2c8bcf77db9e46
Meng Fung <meng.fung@gmail.com>
2012-12-21 01:48:41 -0800

    refactored binary search for array

--- binary_array_search/binary_array_search.rb
+++ binary_array_search/binary_array_search.rb
@@ -1,16 +1,28 @@
 class Array
   def binary_find(num = nil)
-    if self.length % 2 == 0
+    if length_even
       self << nil
     end
-    if self[self.length/2] == num
+    if self[mid_index] == num
       return true
-    elsif self.length == 1 || self[self.length/2] == nil
+    elsif self.length == 1 || self[mid_index] == nil
       return false
     else
-      mid_value = self[self.length/2]
-      mid_index = self.length/2
       mid_value > num ? self[0...mid_index].binary_find(num) : self[(mid_index+1)..self.length].binary_find(num)
     end
+  end
+  
+  private
+  
+  def mid_value
+    self[self.length/2]
+  end
+  
+  def mid_index
+    self.length/2
+  end
+  
+  def length_even
+    self.length % 2 == 0 ? true : false
   end
 end
