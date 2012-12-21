964b2ef26992e181a37109593bfc9a0fa11650a3
Meng Fung <meng.fung@gmail.com>
2012-12-21 01:36:51 -0800

    single pass O(n)

--- max_subarray_problem/max_subarray.rb
+++ max_subarray_problem/max_subarray.rb
@@ -1,5 +1,10 @@
 class Array
   def max_subarray_sum
-    
+    max_here = max_now = 0
+    self.each do |element|
+      max_here = [0,(max_here + element)].max
+      max_now = [max_now,max_here].max
+    end
+    return max_now
   end
 end
--- max_subarray_problem/max_subarray_spec.rb
+++ max_subarray_problem/max_subarray_spec.rb
@@ -8,4 +8,34 @@ describe 'Max Sub Array' do
     Given(:array) { [] }
     Then { expect(array.respond_to?(:max_subarray_sum)).to eql true }
   end
+  
+  context '1 item array' do
+    Given(:array) { [1] }
+    When(:result) { array.max_subarray_sum }
+    Then { expect(result).to eql 1 }
+  end
+  
+  context '2 item array' do
+    Given(:array) { [1,2] }
+    When(:result) { array.max_subarray_sum }
+    Then { expect(result).to eql 3 }
+  end
+  
+  context 'complex 3 item array' do
+    Given(:array) { [1,2,-3] }
+    When(:result) { array.max_subarray_sum }
+    Then { expect(result).to eql 3 }
+  end
+  
+  context 'complex array' do
+    Given(:array) { [-12,2,3,-23,4,-2,67,-3] }
+    When(:result) { array.max_subarray_sum }
+    Then { expect(result).to eql 69 }
+  end
+  
+  context 'another complex array' do
+    Given(:array) { [-2,1,-3,4,-1,2,1,-5,4] }
+    When(:result) { array.max_subarray_sum }
+    Then { expect(result).to eql 6 }
+  end
 end
