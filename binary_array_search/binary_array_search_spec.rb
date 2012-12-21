424dcd2177c5facaac687ac1772a95569428b5b5
Meng Fung <meng.fung@gmail.com>
2012-12-21 01:00:17 -0800

    refactored spec

--- binary_array_search/binary_array_search_spec.rb
+++ binary_array_search/binary_array_search_spec.rb
@@ -10,76 +10,76 @@ describe 'Binary Array Search' do
     When(:result) { array.binary_find(search_number) }
     Then { expect(result).to eql false }
   end
-  context '1 item array with match' do
+  context '1 item array' do
     Given(:array) { [1] }
-    Given(:search_number) { 1 }
-    When(:result) { array.binary_find(search_number) }
-    Then { expect(result).to eql true }
+    context 'with match' do
+      Given(:search_number) { 1 }
+      When(:result) { array.binary_find(search_number) }
+      Then { expect(result).to eql true }
+    end
+    context 'with no match' do
+      Given(:search_number) { 3 }
+      When(:result) { array.binary_find(search_number) }
+      Then { expect(result).to eql false }
+    end
   end
-  context '1 item array with no match' do
-    Given(:array) { [1] }
-    Given(:search_number) { 3 }
-    When(:result) { array.binary_find(search_number) }
-    Then { expect(result).to eql false }
-  end
-  context '2 item array with no match' do
+  context '2 item array' do
     Given(:array) { [1,2] }
-    Given(:search_number) { 3 }
-    When(:result) { array.binary_find(search_number) }
-    Then { expect(result).to eql false }
+    context 'with no match' do
+      Given(:search_number) { 3 }
+      When(:result) { array.binary_find(search_number) }
+      Then { expect(result).to eql false }
+    end
+    context 'match on right' do
+      Given(:search_number) { 2 }
+      When(:result) { array.binary_find(search_number) }
+      Then { expect(result).to eql true }
+    end
+    context 'match on left' do
+      Given(:search_number) { 1 }
+      When(:result) { array.binary_find(search_number) }
+      Then { expect(result).to eql true }
+    end
   end
-  context '2 item array with match on right' do
-    Given(:array) { [1,2] }
-    Given(:search_number) { 2 }
-    When(:result) { array.binary_find(search_number) }
-    Then { expect(result).to eql true }
-  end
-  context '2 item array with match on left' do
-    Given(:array) { [1,2] }
-    Given(:search_number) { 1 }
-    When(:result) { array.binary_find(search_number) }
-    Then { expect(result).to eql true }
-  end
-  context '3 item array with no match' do
+  context '3 item array' do
     Given(:array) { [1,2,3] }
-    Given(:search_number) { 4 }
-    When(:result) { array.binary_find(search_number) }
-    Then { expect(result).to eql false }
+    context '3 item array with no match' do
+      Given(:search_number) { 4 }
+      When(:result) { array.binary_find(search_number) }
+      Then { expect(result).to eql false }
+    end
+    context '3 item array with match in middle' do
+      Given(:search_number) { 2 }
+      When(:result) { array.binary_find(search_number) }
+      Then { expect(result).to eql true }
+    end
+    context '3 item array with match in right' do
+      Given(:search_number) { 3 }
+      When(:result) { array.binary_find(search_number) }
+      Then { expect(result).to eql true }
+    end
+    context '3 item array with match in left' do
+      Given(:search_number) { 1 }
+      When(:result) { array.binary_find(search_number) }
+      Then { expect(result).to eql true }
+    end
   end
-  context '3 item array with match in middle' do
-    Given(:array) { [1,2,3] }
-    Given(:search_number) { 2 }
-    When(:result) { array.binary_find(search_number) }
-    Then { expect(result).to eql true }
-  end
-  context '3 item array with match in right' do
-    Given(:array) { [1,2,3] }
-    Given(:search_number) { 3 }
-    When(:result) { array.binary_find(search_number) }
-    Then { expect(result).to eql true }
-  end
-  context '3 item array with match in left' do
-    Given(:array) { [1,2,3] }
-    Given(:search_number) { 1 }
-    When(:result) { array.binary_find(search_number) }
-    Then { expect(result).to eql true }
-  end
-  context 'Complex array with no match' do
+  context 'Complex array' do
     Given(:array) { [1,2,3,5,6,7,9,10,11,23,34,56] }
-    Given(:search_number) { 76 }
-    When(:result) { array.binary_find(search_number) }
-    Then { expect(result).to eql false }
-  end
-  context 'Complex array with match on left' do
-    Given(:array) { [1,2,3,5,6,7,9,10,11,23,34,56] }
-    Given(:search_number) { 3 }
-    When(:result) { array.binary_find(search_number) }
-    Then { expect(result).to eql true }
-  end
-  context 'Complex array with match on right' do
-    Given(:array) { [1,2,3,5,6,7,9,10,11,23,34,56] }
-    Given(:search_number) { 34 }
-    When(:result) { array.binary_find(search_number) }
-    Then { expect(result).to eql true }
+    context 'Complex array with no match' do
+      Given(:search_number) { 76 }
+      When(:result) { array.binary_find(search_number) }
+      Then { expect(result).to eql false }
+    end
+    context 'Complex array with match on left' do
+      Given(:search_number) { 3 }
+      When(:result) { array.binary_find(search_number) }
+      Then { expect(result).to eql true }
+    end
+    context 'Complex array with match on right' do
+      Given(:search_number) { 34 }
+      When(:result) { array.binary_find(search_number) }
+      Then { expect(result).to eql true }
+    end
   end
 end
